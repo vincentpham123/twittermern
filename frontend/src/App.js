661a81ab1875f566074fe9a761318a6e3ce700f2
vincentpham123 <vincentpham32@gmail.com>
2023-07-03 01:48:18 -0700

    set up tweets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,10 @@ function App() {
         <AuthRoute exact path="/" component={MainPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
+
+        <ProtectedRoute exact path="/tweets" component={Tweets} />
+        <ProtectedRoute exact path="/profile" component={Profile} />
+        <ProtectedRoute exact path="/tweets/new" component={TweetCompose} />
       </Switch>
     </>
   );
