a6773dcb87d6d76de0d98bd3810222f590260364
vincentpham123 <vincentpham32@gmail.com>
2023-07-02 22:54:14 -0700

    set up project for react

--- backend/routes/api/tweets.js
+++ backend/routes/api/tweets.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
-    message: "GET /users"
+    message: "GET /api/tweets"
   });
 });
 
