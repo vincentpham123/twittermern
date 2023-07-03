b155bf1c6b8ab64bb5d32462d374457ed2283141
vincentpham123 <vincentpham32@gmail.com>
2023-07-03 01:38:16 -0700

    set up navbar and routes in app.js

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+function MainPage() {
+    return (
+        <>
+            <p>A Twitter Clone</p>
+            <footer>
+                Copyright &copy; 2023 chirper
+            </footer>
+        </>
+    )
+}
+export default MainPage;
